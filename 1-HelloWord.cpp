c99fac33793e50aefd155a8a4bd4c4b32360c565
Danatielly da Costa Pereira <danatilly@gmail.com>
2020-10-11 12:54:24 -0300

    corrigindo erros de digitacao

--- 1-HelloWord.cpp
+++ 1-HelloWord.cpp
@@ -2,7 +2,7 @@
 #include<conio.h>
 #include<stdio.h>
 // Iniciando os estudos de algoritmos em C 
-// Primeiro coigo teste hello Word
+// Primeiro codigo teste hello Word
 int main ()
 {
 	printf("Hello Word !");
